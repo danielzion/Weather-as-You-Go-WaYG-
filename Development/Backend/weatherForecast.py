f246b7a0e9230520c91d8d550137454a41448f19
mkyd-kill <romeomureithi@gmail.com>
2022-11-15 03:37:12 +0300

    Faster page rendering

--- Development/Backend/weatherForecast.py
+++ Development/Backend/weatherForecast.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, jsonify, render_template
+from flask import Flask, abort, render_template
 from configparser import ConfigParser
 import requests
 from os import path
@@ -7,12 +7,12 @@ from os import path
 # the app
 app = Flask(__name__, instance_relative_config=True)
 
-# icons folder
 ICONS = path.join("static", "icons")
 
 app.config['SECRET_KEY'] = "ThisIsASecretDonTel"
 app.config['SESSION_TYPE'] = "filesystem"
 app.config['UPLOAD_FOLDER'] = ICONS
+
 
 # getting the weather API key
 config_file = 'key.ini'
@@ -24,6 +24,7 @@ weather_api = config['api_key']['key']
 # main route
 @app.route("/show-forecast/<city>", methods=['GET'])
 def getForecast(city):
+    
     # city = request.args.get('city')
 
     if city:
@@ -41,51 +42,16 @@ def getForecast(city):
 
             forecast_response = requests.get(weather_url, params=url_params).json()
 
-            final_result = format_weather(forecast_response)
-
-            return final_result
-
         except Exception as error:
             return error
     else:
         abort(400, "City Argument Not Found")
 
-
-
-# formatting the weather json
-def format_weather(weatherJson):
-    try:
-        city_name = weatherJson['city']['name']
-        city_country = weatherJson['city']['country']
-        for item in weatherJson['list']:
-            weather_description = item['weather'][0]['description']
-            weather_temperature = item['main']['temp']
-            weather_humidity = item['main']['humidity']
-            wind_speed = item['wind']['speed']
-            weather_icon = item['weather'][0]['icon']
-            forecast_date = item['dt_txt']
-
-        data_result = {
-            'City': city_name,
-            'Country': city_country,
-            'Description': weather_description,
-            'Temperature': weather_temperature,
-            'Humidity': weather_humidity,
-            'Wind Speed': wind_speed,
-            'Icon': weather_icon,
-            'Date': forecast_date
-        }
-
-        data_response = jsonify(data_result)
-
-    except Exception as error:
-        abort(400, error)
-
-    return data_response
+    return render_template('index.html', title="Weather Forecast", weatherJson=forecast_response)
 
 
 
-#@app.route("/show-coordinates/<city>")
+# getting coordinated to be passed as args in the forecast api
 def get_latitude_longitude(city):
     try:
         city_url = 'https://api.openweathermap.org/geo/1.0/direct'
