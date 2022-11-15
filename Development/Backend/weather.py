4b0221417c2c0d5d71613505138684f44e3212b2
mkyd-kill <romeomureithi@gmail.com>
2022-11-15 03:37:41 +0300

    Fixed error

--- Development/Backend/weather.py
+++ Development/Backend/weather.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, abort, Response
+from flask import Flask, render_template, request, abort, Response, json
+import urllib
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
-def index():
+@app.route('/<city>', methods=["GET"])
+def index(city):
 
     city = request.args.get('city')
     if city is None:
@@ -15,10 +16,13 @@ def index():
     data['units'] = 'metric'
     url_values = urllib.parse.urlencode(data)
     url = 'http://api.openweathermap.org/data/2.5/forecast'
-    full_url - url + '?' + url_values
+    full_url = url + '?' + url_values
     data = urllib.request.urlopen(full_url)
 
     resp = Response(data)
     resp.status_code = 200
     return render_template('index.html', title="Weather App", data=json.loads(data.read().decode("utf8")))
+
+
+app.run(debug=True)
     
