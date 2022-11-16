02379e85bd53ea5232ef559891f8fad0e5a094ea
mkyd-kill <romeomureithi@gmail.com>
2022-11-17 01:20:01 +0300

    Initialised scrapper

---
+++ Development/Backend/news_scrapper.py
@@ -0,0 +1,7 @@
+# news web scrapper
+from bs4 import BeautifulSoup
+import requests
+
+# scrap weather news articles from the web (from a popular website like nasa)
+# get the relevant html tags as text
+# integrate the scrapped html tags into our system
